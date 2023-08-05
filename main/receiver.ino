ce940d6655f685a87a3cfd1f1822d8ae92012cf4
Amar Chauhan <chauhanamarsingh@gmail.com>
2023-08-05 11:52:50 +0530

    v3

--- main/receiver.ino
+++ main/receiver.ino
@@ -1,12 +1,5 @@
 /*
-  Rui Santos
-  Complete project details at Complete project details at https://RandomNerdTutorials.com/esp8266-nodemcu-http-get-post-arduino/
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-  
-  Code compatible with ESP8266 Boards Version 3.0.0 or above 
-  (see in Tools > Boards > Boards Manager > ESP8266)
+  Connect to WIFI and wait for serial input, If serial input recived, post data to server.
 */
 
 #include <ESP8266WiFi.h>
@@ -20,17 +13,9 @@ String value="0";
 //Your Domain name with URL path or IP address with path
 const char* serverName = "http://bhupali.000webhostapp.com";
 
-// the following variables are unsigned longs because the time, measured in
-// milliseconds, will quickly become a bigger number than can be stored in an int.
-unsigned long lastTime = 0;
-// Timer set to 10 minutes (600000)
-//unsigned long timerDelay = 600000;
-// Set timer to 5 seconds (5000)
-unsigned long timerDelay = 5000;
-
 void setup() {
   Serial.begin(115200);
-
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
   while(WiFi.status() != WL_CONNECTED) {
@@ -40,49 +25,42 @@ void setup() {
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
- 
-  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
 }
 
-void loop() {  
-  
-  //Send an HTTP POST request every timerDelay seconds
-  if ((millis() - lastTime) > timerDelay) {
-     if(Serial.available()>0)    //Checks is there any data in buffer 
+void loop() {
+  if(Serial.available()>0)    //Checks is there any data in buffer 
           {
             //char value = Serial.read();
             value =Serial.readString();
             Serial.print("Reading:");
             Serial.print(value);  //Read serial data byte and send back to serial monitor
+                //Check WiFi connection status
+            if(WiFi.status()== WL_CONNECTED){
+              WiFiClient client;
+              HTTPClient http;
+              
+              // Your Domain name with URL path or IP address with path
+              http.begin(client, serverName);          
+              // Specify content-type header
+              http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+              // Data to send with HTTP POST
+              String httpRequestData = "api_key=tPmAT5Ab3j7F9&level=" + value;       
+              // Send HTTP POST request
+              int httpResponseCode = http.POST(httpRequestData);
+                   
+              Serial.print("HTTP Response code: ");
+              Serial.println(httpResponseCode);
+                              
+              // Free resources
+              http.end();
+              digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+              delay(50);                      // Wait for a second
+              digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+              delay(950);  
+  
+            }
+            else {
+              Serial.println("WiFi Disconnected");
+            }
           }
-    //Check WiFi connection status
-    if(WiFi.status()== WL_CONNECTED){
-      WiFiClient client;
-      HTTPClient http;
-      
-      // Your Domain name with URL path or IP address with path
-      http.begin(client, serverName);
-  
-      // If you need Node-RED/server authentication, insert user and password below
-      //http.setAuthorization("REPLACE_WITH_SERVER_USERNAME", "REPLACE_WITH_SERVER_PASSWORD");
-  
-      // Specify content-type header
-      http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-      // Data to send with HTTP POST
-      String httpRequestData = "api_key=tPmAT5Ab3j7F9&level=" + value;
-      Serial.print(httpRequestData);           
-      // Send HTTP POST request
-      int httpResponseCode = http.POST(httpRequestData);
-           
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
-        
-      // Free resources
-      http.end();
-    }
-    else {
-      Serial.println("WiFi Disconnected");
-    }
-    lastTime = millis();
-  }
 }
--- main/transmitter.ino
+++ main/transmitter.ino
@@ -1,35 +1,80 @@
-/*
- * ESP8266 Deep sleep mode example
- * Rui Santos 
- * Complete Project Details https://randomnerdtutorials.com
- */
-const int analogInPin = A0;  // ESP8266 Analog Pin ADC0 = A0
-int sensorValue = 0;  // value read from the pot
- 
-void setup() {
+//https://www.circuitbasics.com/how-to-make-an-arduino-capacitance-meter/
+
+
+const int OUT_PIN = PA4;
+const int IN_PIN = PA5;
+const float IN_STRAY_CAP_TO_GND = 24.48;
+const float IN_CAP_TO_GND  = IN_STRAY_CAP_TO_GND;
+const float R_PULLUP = 34.8;  
+//const int MAX_ADC_VALUE = 1023;
+const int MAX_ADC_VALUE = 4095;
+
+void setup(){
+  pinMode(OUT_PIN, OUTPUT);
+  pinMode(IN_PIN, OUTPUT);
   Serial.begin(115200);
-  Serial.setTimeout(2000);
-
-  // Wait for serial to initialize.
-  while(!Serial) { }
-  
-  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
-  digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
-  delay(1000);                      // Wait for a second
-  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
-  delay(1000);                      // Wait for two seconds (to demonstrate the active low LED)
-  sensorValue = analogRead(analogInPin);
-  Serial.println(sensorValue);
-  
-  
-
-  
-  // Deep sleep mode for 30 seconds, the ESP8266 wakes up by itself when GPIO 16 (D0 in NodeMCU board) is connected to the RESET pin
-  //Serial.println("I'm awake, but I'm going into deep sleep mode for 58 seconds");
-  ESP.deepSleep(58e6); 
-  
-  // Deep sleep mode until RESET pin is connected to a LOW signal (for example pushbutton or magnetic reed switch)
 }
 
-void loop() {
+void loop(){
+    pinMode(IN_PIN, INPUT);
+    digitalWrite(OUT_PIN, HIGH);
+    int val = analogRead(IN_PIN);
+    digitalWrite(OUT_PIN, LOW);
+
+    if (val < 3500){
+      pinMode(IN_PIN, OUTPUT);
+
+      float capacitance = (float)val * IN_CAP_TO_GND / (float)(MAX_ADC_VALUE - val);
+
+      Serial.print(F("Capacitance Value = "));
+      Serial.print(capacitance, 3);
+      Serial.print(F(" pF ("));
+      Serial.print(val);
+      Serial.println(F(") "));
+    }
+
+    else{
+      pinMode(IN_PIN, OUTPUT);
+      delay(1);
+      pinMode(OUT_PIN, INPUT_PULLUP);
+      unsigned long u1 = micros();
+      unsigned long t;
+      int digVal;
+
+      do{
+        digVal = digitalRead(OUT_PIN);
+        unsigned long u2 = micros();
+        t = u2 > u1 ? u2 - u1 : u1 - u2;
+      } while ((digVal < 1) && (t < 400000L));
+
+      pinMode(OUT_PIN, INPUT);  
+      val = analogRead(OUT_PIN);
+      digitalWrite(IN_PIN, HIGH);
+      int dischargeTime = (int)(t / 1000L) * 5;
+      delay(dischargeTime);   
+      pinMode(OUT_PIN, OUTPUT);  
+      digitalWrite(OUT_PIN, LOW);
+      digitalWrite(IN_PIN, LOW);
+
+      float capacitance = -(float)t / R_PULLUP / log(1.0 - (float)val / (float)MAX_ADC_VALUE);
+
+      Serial.print(F("Capacitance Value1 = "));
+      if (capacitance > 1000.0){
+        Serial.print(capacitance / 1000.0, 2);
+        Serial.print(F(" uF"));
+      }
+      else{
+        Serial.print(capacitance, 2);
+        Serial.print(F(" nF"));
+      }
+// Commented extra code that was not needed
+      Serial.print(F(" ("));
+      Serial.print(digVal == 1 ? F("Normal") : F("HighVal"));
+      Serial.print(F(", t= "));
+      Serial.print(t);
+      Serial.print(F(" us, ADC= "));
+      Serial.print(val);
+      Serial.println(F(")"));
+    }
+    while (millis() % 1000 != 0);    
 }
